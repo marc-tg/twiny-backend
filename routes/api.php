52b61cb7e550924c08846ca17cd9ca80f7eb08f6
marc-tg <marctg2005@gmail.com>
2025-08-08 17:46:11 +0200

    Laravel works

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,5 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::prefix('v1')->group(function () {
-    Route::resource('users', 'Api\v1\UserController');
+    Route::resource('users', 'UserController');
 
-});
