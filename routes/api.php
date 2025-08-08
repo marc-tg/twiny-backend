491e1107472056d693b5b3f817201eabefc71c28
marc-tg <marctg2005@gmail.com>
2025-08-09 01:38:45 +0200

    Likes created

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\PostController;
-
+use App\Http\Controllers\LikeController;
 
     Route::resource('users', 'UserController');
     Route::resource('posts', 'PostController');
-    Route::get('posts/{id}', [PostController::class, 'show']);
+    Route::get('likes/{id}', [LikeController::class, 'show']);
